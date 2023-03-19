c9655b7a7180847ddb9e94801775485baef2d5bb
Fireye025 <43316468+Fireye025@users.noreply.github.com>
2023-03-19 14:45:36 +0100

    test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const scores = db.collection("scores")
 // #############################################################################
 
 app.get('/scores', async (req, res) => {
-    const items = await scores.list()
+    const items = await scores.get('Fireye025')
     console.log(JSON.stringify(items))
     res.json(items).end()
   })
