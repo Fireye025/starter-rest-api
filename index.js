dbad660c6b7ee27a0d318d17fe0d44f65ab1b410
Fireye025 <43316468+Fireye025@users.noreply.github.com>
2023-03-19 14:41:21 +0100

    hopefully works

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/scores', async (req, res) => {
   })
 
 app.post('/scores', async (req, res) => {
-    const item = await scores.set(req.body.name, req.body.props.score)
+    const item = await scores.set(req.body.name, {score: req.body.props.score})
     console.log(JSON.stringify(item))
     res.json(item).end()
   })
