650d494a74c0afc8cc5ebb2aa13975351620a93f
Fireye025 <43316468+Fireye025@users.noreply.github.com>
2023-03-19 15:07:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,12 +23,13 @@ const scores = db.collection("scores")
 
 app.get('/scores', async (req, res) => {
     const items = await scores.list()
-    for(var item in items.results){
-      if(item.collection == "scores"){
-        console.log(item.key)
+    var data = JSON.stringify(items)
+    for(var item in data["results"]){
+      if(item["collection"] == "scores"){
+        console.log(item["key"])
       }
     }
-    console.log(JSON.stringify(items))
+    console.log(JSON.stringify(items)["results"])
     res.json(items).end()
   })
 
