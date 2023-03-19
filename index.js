d47aaf292a9a750a00cad9fff41c97d58b30b368
Fireye025 <43316468+Fireye025@users.noreply.github.com>
2023-03-19 14:33:51 +0100

    clean

--- index.js
+++ index.js
@@ -21,56 +21,15 @@ const scores = db.collection("scores")
 // app.use(express.static('public', options))
 // #############################################################################
 
-// Create or Update an item
-// app.post('/:col/:key', async (req, res) => {
-//   console.log(req.body)
-
-//   const col = req.params.col
-//   const key = req.params.key
-//   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-//   const item = await db.collection(col).set(key, req.body)
-//   console.log(JSON.stringify(item, null, 2))
-//   res.json(item).end()
-// })
-
-// // Delete an item
-// app.delete('/:col/:key', async (req, res) => {
-//   const col = req.params.col
-//   const key = req.params.key
-//   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-//   const item = await db.collection(col).delete(key)
-//   console.log(JSON.stringify(item, null, 2))
-//   res.json(item).end()
-// })
-
-// // Get a single item
-// app.get('/:col/:key', async (req, res) => {
-//   const col = req.params.col
-//   const key = req.params.key
-//   console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
-//   const item = await db.collection(col).get(key)
-//   console.log(JSON.stringify(item, null, 2))
-//   res.json(item).end()
-// })
-
-// // Get a full listing
-// app.get('/:col', async (req, res) => {
-//   const col = req.params.col
-//   console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
-//   const items = await db.collection(col).list()
-//   console.log(JSON.stringify(items, null, 2))
-//   res.json(items).end()
-// })
-
 app.get('/scores', async (req, res) => {
     const items = await scores.list()
-    console.log(JSON.stringify(items, null, 2))
+    console.log(JSON.stringify(items))
     res.json(items).end()
   })
 
 app.post('/scores', async (req, res) => {
     const item = await scores.set(req.body.name, req.body.score)
-    console.log(JSON.stringify(item, null, 2))
+    console.log(JSON.stringify(item))
     res.json(item).end()
   })
 
